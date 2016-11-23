5e5f40fef4d6c215bd9e8339ac8fc4923d601112
Naalik <lormir.panther@gmail.com>
2016-11-23 16:35:43 +0100

    tutu

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,21 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
+    assert_select "title", "home"
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_select "title", "help"
     assert_response :success
   end
 
   test "should get about" do
     get static_pages_about_url
+    assert_select "title", "about"
     assert_response :success
   end
 
   test "should get toto" do
     get static_pages_toto_url
+    assert_select "title", "toto"
     assert_response :success
   end
 
