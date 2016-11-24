6195ad10b7dd30ac3a1331bb6986aae07054965b
Naalik <lormir.panther@gmail.com>
2016-11-24 11:26:48 +0100

    css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
-    assert_select "title", "home"
+    assert_select "title", "home | Ma super application"
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
-    assert_select "title", "help"
+    assert_select "title", "help | Ma super application"
     assert_response :success
   end
 
   test "should get about" do
     get static_pages_about_url
-    assert_select "title", "about"
+    assert_select "title", "about | Ma super application"
     assert_response :success
   end
 
   test "should get toto" do
     get static_pages_toto_url
-    assert_select "title", "toto"
+    assert_select "title", "toto | Ma super application"
     assert_response :success
   end
 
